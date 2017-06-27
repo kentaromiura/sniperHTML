e2d60a2b269df41601c94a4de57a97a7f72325c6
Cristian Carlesso <kentaromiura@gmail.com>
2017-06-27 18:27:10 +0100

    Remove not needed stuff

--- hyperHTML.Component.js
+++
@@ -1,8 +0,0 @@
-const hyperHTML = require('hyperhtml');
-hyperHTML.Component = function () {
-  const newTarget = this.__proto__.constructor;
-  return Reflect.construct(HTMLElement, arguments, newTarget);
-}
-
-Object.setPrototypeOf(hyperHTML.Component, HTMLElement);
-Object.setPrototypeOf(hyperHTML.Component.prototype, HTMLElement.prototype);
